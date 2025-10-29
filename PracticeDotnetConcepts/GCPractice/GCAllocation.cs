a56bd29d60ad62122b09f15f7393cf5e9c7659d3
SanketShahane8 <sanket.shahane@outlook.com>
2025-10-29 08:29:38 +0530

    Added gitignore

--- PracticeDotnetConcepts/GCPractice/GCAllocation.cs
+++ PracticeDotnetConcepts/GCPractice/GCAllocation.cs
@@ -3,13 +3,15 @@ using System;
 class GCAllocation
 {
     public static byte[] data { get; private set; }
+    public static byte[] data2 { get; private set; }
 
     public static void AllocateMemory(int sizeInMB = 10)
     {
         Console.WriteLine("\nAllocating memory...");
-        
+
         ///-- Allocate large array memory
         data = new byte[sizeInMB * 1024 * 1024]; // Allocate specified MB
+        data2 = new byte[2 * sizeInMB * 1024 * 1024]; // Allocate specified MB * 2
 
         Console.WriteLine("Allocation complete.");
     }
